5991e5342fe82a3aa180e3e5974d1b7deb23d988
shukubota <kubotashu0421@gmail.com>
2022-06-15 21:00:41 +0900

    main関数作る

---
+++ majong/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func test() {
+	fmt.Println("fff")
+}
+
+type Tile struct {
+	number   int
+	tileType string
+}
+
+func main() {
+	// struct Hai {
+	// 	number: int
+	// }
+
+	test()
+}
