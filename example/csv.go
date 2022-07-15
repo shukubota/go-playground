f26e12a9014e8197ba36c9201fb1a5591015d7ac
shukubota <kubotashu0421@gmail.com>
2022-07-15 19:14:46 +0900

    byteカット

--- example/csv.go
+++ example/csv.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func afterRead(file *os.File) {
 	file.Close()
+}
+
+var UTF8_BOM = []byte{239, 187, 191}
+
+func hasBOM(in []byte) bool {
+	return bytes.HasPrefix(in, UTF8_BOM)
+}
+
+func stripBOM(in []byte) []byte {
+	return bytes.TrimPrefix(in, UTF8_BOM)
 }
 
 func readCSV() {
@@ -20,7 +33,37 @@ func readCSV() {
 	defer afterRead(file)
 
 	fmt.Println(file)
-	fmt.Println("test")
+
+	// nil, 0 (not bom)
+	fmt.Println("00sssss")
+	var row []byte
+	row, err = io.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+		log.Panic("failed to read file")
+	}
+
+	hasBom := hasBOM(row)
+	fmt.Printf("hasbom: %v\n", hasBom)
+
+	var rs string
+	if hasBom {
+		rs = string(stripBOM(row))
+	} else {
+		rs = string(row)
+	}
+
+	fmt.Println(row)
+	fmt.Println(rs)
+
+	a := strings.Split(rs, "\r\n")
+	fmt.Println(a)
+	fmt.Println(len(a))
+	for _, memberId := range a {
+		fmt.Println(memberId)
+		fmt.Println(memberId == "Tansu1111member")
+	}
+
 }
 
 func main() {
