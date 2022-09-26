d6a138bd24262c888bb3a93aaa34d52274157ddb
shukubota <kubotashu0421@gmail.com>
2022-09-26 19:52:53 +0900

    scraping用のhandler用意

--- serverless-template/searchContent/handler/handler.go
+++ serverless-template/searchContent/handler/handler.go
@@ -1,12 +1,35 @@
 package handler
 
 import (
+	"context"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/aws/aws-lambda-go/events"
+	"log"
+	"net/http"
 )
 
-func Handler() (bool, error) {
+func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	fmt.Println("----------sssss")
 	// ここにscraping処理
 
-	return true, nil
+	res, err := http.Get("https://qiita.com/timeline")
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	fmt.Println(res)
+	fmt.Println(err)
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("===========doc")
+	fmt.Println(doc)
+
+	return events.APIGatewayProxyResponse{}, nil
 }
--- serverless-template/searchContent/sandbox/sandbox.go
+++ serverless-template/searchContent/sandbox/sandbox.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/shukubota/go-api-template/serverless-template/searchContent/handler"
 	"log"
 )
 
 func main() {
-	r, err := handler.Handler()
+	req := events.APIGatewayProxyRequest{}
+
+	r, err := handler.Handler(context.Background(), req)
 	fmt.Println(r)
 	if err != nil {
 		log.Fatalln("handler error")
