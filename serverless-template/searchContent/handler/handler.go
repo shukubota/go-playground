504931cfe58a76f09d6876bab230b6120f5f6470
shukubota <kubotashu0421@gmail.com>
2022-09-29 21:03:28 +0900

    成績データ取得サンプル

--- serverless-template/searchContent/handler/handler.go
+++ serverless-template/searchContent/handler/handler.go
@@ -2,12 +2,39 @@ package handler
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/aws/aws-lambda-go/events"
+	"io"
 	"log"
 	"net/http"
 )
+
+type Result struct {
+	Name string              `json:"name""`
+	Rate map[int]int         `json:"rate""`
+	List []map[string]string `json:"list"'`
+}
+
+//{
+//"starttime": "1661871720",
+//"during": "11",
+//"sctype": "b",
+//"playernum": "4",
+//"playerlevel": "0",
+//"playlength": "1",
+//"kuitanari": "1",
+//"akaari": "1",
+//"player1": "ゆkんm",
+//"player1ptr": "58.1",
+//"player2": "エイチデニウソン",
+//"player2ptr": "4.3",
+//"player3": "ハゲ村の会長",
+//"player3ptr": "-15.7",
+//"player4": "337tn101",
+//"player4ptr": "-46.7"
+//},
 
 func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	fmt.Println("----------sssss")
@@ -20,6 +47,25 @@ func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events
 		return events.APIGatewayProxyResponse{}, err
 	}
 
+	req, err := http.NewRequest("GET", "https://nodocchi.moe/api/listuser.php", nil)
+
+	params := req.URL.Query()
+	params.Add("name", "ハゲ村の会長")
+	req.URL.RawQuery = params.Encode()
+
+	fmt.Println(req.URL.String())
+
+	client := &http.Client{}
+	r, err := client.Do(req)
+
+	body, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	var b Result
+	json.Unmarshal(body, &b)
+
+	fmt.Println(b)
+	fmt.Println("status")
 	fmt.Println(res)
 	fmt.Println(err)
 
