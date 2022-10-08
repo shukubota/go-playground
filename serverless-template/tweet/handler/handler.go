86b5d288e464848d92e9d4a833715b4367cedd61
shukubota <kubotashu0421@gmail.com>
2022-10-08 16:44:24 +0900

    動画upload

--- serverless-template/tweet/handler/handler.go
+++ serverless-template/tweet/handler/handler.go
@@ -8,7 +8,6 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"io"
 	"log"
-	"math"
 	"net/http"
 	"net/url"
 	"os"
@@ -40,43 +39,63 @@ func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events
 	anaconda.SetConsumerKey(apiKey)
 	anaconda.SetConsumerSecret(apiSecret)
 	api := anaconda.NewTwitterApi(accessToken, accessTokenSecret)
-	text := "Test Tweet"
+	text := "良さげ"
 
 	fmt.Println(api)
 	fmt.Println(text)
 
-	im, err := http.Get("https://cc3001.dmm.co.jp/litevideo/freepv/h/hoi/hoisw00018/hoisw00018_dmb_w.mp4")
+	im, err := http.Get("https://sample.mgstage.com/sample/manmanland/476mla/075/476mla-075_20220308T135029.mp4")
+	//im, err := http.Get("https://cc3001.dmm.co.jp/litevideo/freepv/h/hoi/hoisw00018/hoisw00018_dmb_w.mp4")
 	fmt.Println(im)
 
 	if err != nil {
 		return events.APIGatewayProxyResponse{}, err
 	}
 
-	data, err := io.ReadAll(im.Body)
+	bytes, err := io.ReadAll(im.Body)
 	defer im.Body.Close()
-
-	//base64Data := base64.StdEncoding.EncodeToString(body)
-
-	//fmt.Println(base64Data)
-
-	//var base64String = "/9j/4AAQSk...LT09DojFy1Z//2Q=="
-	//media, _ := api.UploadMedia(base64Data)
-	media, err := api.UploadVideoInit(len(data), "video/mp4")
+	totalBytes := len(bytes)
+	media, err := api.UploadVideoInit(totalBytes, "video/mp4")
 	fmt.Println(media)
+	fmt.Println(err)
 
-	chunkIndex := 0
-	for i := 0; i < len(data); i += 5242879 {
-		log.Println("Chunk", chunkIndex)
-		err = api.UploadVideoAppend(media.MediaIDString, chunkIndex,
-			base64.StdEncoding.EncodeToString(
-				data[i:int(math.Min(5242879.0, float64(len(data))))],
-			),
-		)
-		if err != nil {
-			return events.APIGatewayProxyResponse{}, err
+	fmt.Println(len(bytes))
+
+	fmt.Println("=============media")
+
+	//mediaMaxLen := 1 * 1024 * 1024
+	mediaMaxLen := 5000000
+	segment := 0
+	for i := 0; i < totalBytes; i += mediaMaxLen {
+		var mediaData string
+		fmt.Printf("%v\n", segment)
+		if i+mediaMaxLen < totalBytes {
+			mediaData = base64.StdEncoding.EncodeToString(bytes[i : i+mediaMaxLen])
+		} else {
+			mediaData = base64.StdEncoding.EncodeToString(bytes[i:])
 		}
-		chunkIndex++
+		if err = api.UploadVideoAppend(media.MediaIDString, segment, mediaData); err != nil {
+			break
+		}
+		segment += 1
 	}
+
+	//chunkIndex := 0
+	//mediaMaxLen := 5 * 1024 * 1024
+	//for i := 0; i < len(data); i += mediaMaxLen {
+	//	log.Println("Chunk", chunkIndex)
+	//	err = api.UploadVideoAppend(media.MediaIDString, chunkIndex,
+	//		base64.StdEncoding.EncodeToString(
+	//			data[i:int(math.Min(float64(mediaMaxLen), float64(len(data))))],
+	//		),
+	//	)
+	//	if err != nil {
+	//		fmt.Println("=========err")
+	//		fmt.Printf("%v\n", err)
+	//		return events.APIGatewayProxyResponse{}, err
+	//	}
+	//	chunkIndex++
+	//}
 
 	videoMedia, err := api.UploadVideoFinalize(media.MediaIDString)
 	if err != nil {
