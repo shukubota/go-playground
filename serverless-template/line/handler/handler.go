16ab251270cb764bc036ceb44a086350b692de17
shukubota <kubotashu0421@gmail.com>
2022-10-11 01:08:17 +0900

    responseを修正

--- serverless-template/line/handler/handler.go
+++ serverless-template/line/handler/handler.go
@@ -6,9 +6,16 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 )
 
+type Response struct {
+	Message string `json:"message"`
+}
+
 func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	fmt.Println("----------post line handler")
 	fmt.Println(request)
 	// ここに処理
-	return events.APIGatewayProxyResponse{}, nil
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "OK",
+	}, nil
 }
