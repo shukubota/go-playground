9e3de0ec94fb520c8f94564ec8db49cf48f7bf97
shukubota <kubotashu0421@gmail.com>
2023-01-03 21:34:59 +0900

    stack作る

--- example/sandbox/main.go
+++ example/sandbox/main.go
@@ -6,23 +6,47 @@ import (
 )
 
 func main() {
-	fmt.Println("==============oooo")
+	fmt.Println("==============func")
 	input := []MyInt{1, 2, 3, 4}
 	r := f1(input)
 	fmt.Println(r)
 	fmt.Println(r[0])
 	fmt.Printf("%T\n", r[0])
 	fmt.Println(r[0] == "1")
+
+	fmt.Println("==============stack")
+	s := New[string]()
+	s.Push("hello")
+	fmt.Println(s)
 }
 
-func f1[T Stringer](xs []T) []string {
+type Stack[T any] []T
+
+func New[T any]() *Stack[T] {
+	v := make(Stack[T], 0)
+	return &v
+}
+
+func (s *Stack[T]) Push(x T) {
+	*s = append(*s, x)
+}
+
+func (s *Stack[T]) Pop() T {
+	v := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
+	return v
+}
+
+func f1[A Stringer](xs []A) []string {
 	var result []string
 	for _, x := range xs {
+		fmt.Printf("%T\n", x)
 		result = append(result, x.String())
 	}
 	return result
 }
 
+// これだとコンパイルできない
 //func f2(xs []Stringer) []string {
 //	var result []string
 //	for _, x := range xs {
