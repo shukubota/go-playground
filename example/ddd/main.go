6d214aa9abbc2f6f051e08819f7002073390c604
shukubota <kubotashu0421@gmail.com>
2023-02-19 21:58:37 +0900

    package名修正

--- example/ddd/main.go
+++ example/ddd/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"github.com/shukubota/go-api-template/example/ddd/repository"
-	"github.com/shukubota/go-api-template/example/ddd/use_case"
+	"github.com/shukubota/go-api-template/example/ddd/usecase"
 	"log"
 )
 
 func main() {
 	name := "Bob"
-	uc := use_case.NewRegisterUserUseCase(repository.NewRegisterUserRepository())
+	uc := usecase.NewRegisterUserUseCase(repository.NewRegisterUserRepository())
 
 	err := uc.Register(name)
 	if err != nil {
--- example/ddd/usecase/register_user.go
+++ example/ddd/usecase/register_user.go
@@ -1,4 +1,4 @@
-package use_case
+package usecase
 
 import (
 	"github.com/shukubota/go-api-template/example/ddd/domain/entity"
