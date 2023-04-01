f60c4429948fb9e017857ea4b79422c440c7a57c
shukubota <kubotashu0421@gmail.com>
2023-04-01 22:14:11 +0900

    channel使う方法

--- example/goroutine/main.go
+++ example/goroutine/main.go
@@ -8,7 +8,8 @@ import (
 
 func main() {
 	fmt.Println("========aaa")
-	useWaitGroup()
+	//useWaitGroup()
+	useChannel()
 }
 
 func useWaitGroup() {
@@ -29,14 +30,61 @@ func useWaitGroup() {
 	wg.Wait()
 }
 
-func test1() {
+func useChannel() {
+	ch1 := make(chan *testResponse)
+	ch2 := make(chan *testResponse)
+	go test1WithChan(ch1)
+	go test2WithChan(ch2)
+
+	res1 := <-ch1
+	fmt.Println(res1)
+	res2 := <-ch2
+	fmt.Println(res2)
+
+}
+
+type testResponse struct {
+	value any
+}
+
+func test1() *testResponse {
 	fmt.Println("test1 start")
 	time.Sleep(1 * time.Second)
 	fmt.Println("test1 end")
+
+	res := &testResponse{
+		value: "end test1",
+	}
+
+	return res
 }
 
-func test2() {
+func test2() *testResponse {
 	fmt.Println("test2 start")
 	time.Sleep(2 * time.Second)
 	fmt.Println("test2 end")
+
+	res := &testResponse{
+		value: "end test2",
+	}
+
+	return res
+}
+
+func test1WithChan(ch chan *testResponse) {
+	fmt.Println("test1 start")
+	time.Sleep(1 * time.Second)
+	fmt.Println("test1 end")
+	ch <- &testResponse{
+		value: "test1 end",
+	}
+}
+
+func test2WithChan(ch chan *testResponse) {
+	fmt.Println("test2 start")
+	time.Sleep(2 * time.Second)
+	fmt.Println("test2 end")
+	ch <- &testResponse{
+		value: "test2 end",
+	}
 }
