1ffc1479fe8d2527828307a8ef4075f3811c37bb
shukubota <kubotashu0421@gmail.com>
2023-05-02 14:21:18 +0900

    sort

--- example/sort/sort.go
+++ example/sort/sort.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"sort"
 	"time"
 )
@@ -47,6 +48,21 @@ var data = []member{
 		name:      "four",
 		createdAt: toPointer(time.Date(2020, 1, 4, 0, 0, 0, 0, time.UTC)),
 	},
+	{
+		id:        444,
+		name:      "four444",
+		createdAt: toPointer(time.Date(2020, 1, 4, 0, 0, 0, 0, time.UTC)),
+	},
+	{
+		id:        4444,
+		name:      "four444-",
+		createdAt: toPointer(time.Date(2020, 1, 4, 0, 0, 0, 0, time.UTC)),
+	},
+}
+
+type Person struct {
+	Name string
+	Age  int
 }
 
 func main() {
@@ -56,4 +72,25 @@ func main() {
 	})
 
 	fmt.Println(data)
+	people := []Person{
+		{Name: "V", Age: 3},
+		{Name: "K", Age: 3},
+		{Name: "Y", Age: 3},
+		{Name: "A", Age: 4},
+		{Name: "E", Age: 3},
+		{Name: "D", Age: 1},
+		{Name: "C", Age: 3},
+		{Name: "X", Age: 2},
+		{Name: "B", Age: 3},
+	}
+
+	sort.Slice(people, func(i, j int) bool { return people[i].Name < people[j].Name })
+	fmt.Printf("NameでSort(Not-Stable):%+v\n", people)
+
+	sort.Slice(people, func(i, j int) bool { return people[i].Age < people[j].Age })
+	fmt.Printf("AgeでSort(Not-Stable):%+v\n", people)
+
+	a := math.Sin(math.Pi / 3.0)
+	fmt.Println(a)
+	fmt.Println(math.Sqrt(3) * 0.5)
 }
