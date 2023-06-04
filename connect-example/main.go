f5121fd8c17ff7df166a5b2a71d9c0b5d6ee35b3
shukubota <kubotashu0421@gmail.com>
2023-06-05 01:32:09 +0900

    cors設定

--- connect-example/main.go
+++ connect-example/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"connect-example/gen/greet/v1/greetv1connect"
 	"connect-example/handler"
+	"fmt"
+	"github.com/rs/cors"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 	"log"
@@ -17,6 +19,18 @@ func main() {
 
 	mux.Handle(path, handler)
 
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://127.0.0.1:5173"},
+		//AllowOriginFunc: func(origin string) bool {
+		//	return true
+		//},
+		AllowedHeaders: []string{"*"},
+	})
+
+	fmt.Println(c)
+
+	h := c.Handler(h2c.NewHandler(mux, &http2.Server{}))
+
 	log.Println("Starting server on port :18080")
-	log.Fatal(http.ListenAndServe("localhost:18080", h2c.NewHandler(mux, &http2.Server{})))
+	log.Fatal(http.ListenAndServe("localhost:18080", h))
 }
