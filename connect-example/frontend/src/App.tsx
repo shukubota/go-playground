039c907af247142e3542b77709a49a8965f306d1
shukubota <kubotashu0421@gmail.com>
2023-06-05 01:32:17 +0900

    button

--- connect-example/frontend/src/App.tsx
+++ connect-example/frontend/src/App.tsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { createPromiseClient } from "@bufbuild/connect";
+import { createConnectTransport } from "@bufbuild/connect-web";
+
+import { GreetService } from "../gen/greet/v1/greet_connect";
+
 import './App.css'
+import {ServiceType} from "@bufbuild/protobuf";
+
+const transport = createConnectTransport({
+  baseUrl: "http://localhost:18080",
+});
+
+const client = createPromiseClient(GreetService as ServiceType, transport);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const onSubmit = async () => {
+    console.log("onsubmit")
+    const res = await client.greet({ name: "buf" });
+    console.log({ res });
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>hello</p>
+        <button onClick={onSubmit}>submit</button>
     </>
   )
 }
