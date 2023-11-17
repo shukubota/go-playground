49bf660cd4b2d67ac9cf15d934631648f683b8bd
shukubota <kubotashu0421@gmail.com>
2023-11-18 00:56:03 +0900

    io reader example

---
+++ example/io/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	src := flag.String("src", "", "source file path")
+	in := flag.String("i", "flag", "")
+	out := flag.String("o", "stdout", "")
+	dest := flag.String("dest", "", "destination file path")
+
+	flag.Parse()
+
+	fmt.Println(*src)
+	fmt.Println(*in)
+	fmt.Println(*out)
+	fmt.Println(*dest)
+
+	var r io.Reader
+	if *in == "txt" {
+		file, err := os.Open(*src)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		r = file
+	} else {
+		sr := bytes.NewReader([]byte(*src))
+		r = sr
+	}
+
+	w, err := os.Create(*dest)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer w.Close()
+
+	err = toUpper(w, r)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func toUpper(w io.Writer, r io.Reader) error {
+	data, err := io.ReadAll(r)
+	if err != nil {
+		return err
+	}
+
+	_, err = w.Write(bytes.ToUpper(data))
+	if err != nil {
+		return err
+	}
+	return nil
+}
