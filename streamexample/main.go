7859fc636656e9e8b76f9cd2d4568b19efd0bfc3
shukubota <kubotashu0421@gmail.com>
2024-06-27 00:20:18 +0900

    openai繋ぐ

--- streamexample/main.go
+++ streamexample/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"github.com/sashabaranov/go-openai"
 	healthpb "github.com/shukubota/go-playground/streamexample/gen/go"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"io"
 	"log"
 	"net"
+	"os"
 )
 
 func main() {
@@ -42,4 +46,53 @@ func (s *healthServer) Check(ctx context.Context, req *healthpb.CheckRequest) (*
 	return &healthpb.CheckResponse{
 		Message: "OK",
 	}, nil
+}
+
+func (s *healthServer) CheckStream(request *healthpb.CheckRequest, server healthpb.HealthService_CheckStreamServer) error {
+	client := openai.NewClient(os.Getenv("OPEN_AI_API_KEY"))
+	ctx := context.Background()
+	stream, err := client.CreateChatCompletionStream(
+		ctx,
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "Hello!",
+				},
+			},
+			Stream: true,
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	defer stream.Close()
+
+	for {
+		response, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			fmt.Println("\nStream finished")
+			return nil
+		}
+
+		if err != nil {
+			fmt.Printf("\nStream error: %v\n", err)
+			return err
+		}
+
+		m := response.Choices[0].Delta.Content
+
+		err = server.Send(&healthpb.CheckResponse{
+			Message: m,
+		})
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf(response.Choices[0].Delta.Content)
+	}
+
+	return nil
 }
