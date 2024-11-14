dca8f6956f7c9e7b0bbbb24839831dc728df84eb
shukubota <kubotashu0421@gmail.com>
2024-11-14 17:16:50 +0900

    iteratorの例

--- example/iterator/main.go
+++ example/iterator/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"iter"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
@@ -9,10 +12,61 @@ func main() {
 		fmt.Println(i)
 	}
 
+	for i := range numbers() {
+		fmt.Println(i)
+	}
+
+	for name, score := range scores() {
+		fmt.Println(name, score)
+	}
+
+	for prev, next := range Pair(numbers()) {
+		fmt.Println(prev, next)
+	}
+
 }
 
 func iterator(yield func(int) bool) {
 	yield(1)
 	yield(2)
 	yield(3)
+}
+
+func numbers() iter.Seq[int] {
+	return func(yield func(int) bool) {
+		yield(1)
+		yield(2)
+		yield(3)
+	}
+}
+
+func scores() iter.Seq2[string, int] {
+	return func(yield func(string, int) bool) {
+		yield("Alice", 100)
+		yield("Bob", 90)
+		yield("Charlie", 80)
+	}
+}
+
+func Pair[V int](seq iter.Seq[V]) iter.Seq2[V, V] {
+	return func(yield func(V, V) bool) {
+		next, stop := iter.Pull(seq)
+		defer stop()
+
+		var prev V
+		if v, ok := next(); ok {
+			prev = v
+		}
+
+		for {
+			if v, ok := next(); ok {
+				if !yield(prev, v) {
+					break
+				}
+				prev = v
+			} else {
+				break
+			}
+		}
+	}
 }
